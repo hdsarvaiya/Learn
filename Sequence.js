a8e9818b5318c42d56a15dbeb3463e15a0413b64
Harshvardhansinh Sarvaiya <145086716+hdsarvaiya@users.noreply.github.com>
2024-08-10 20:25:52 +0530

    Update Sequence.js

--- Sequence.js
+++ Sequence.js
@@ -1,1 +1,15 @@
 
+function sequence(s, temp, index) {
+    if (index === s.length) {
+        return [temp]; 
+    }
+
+    let take = sequence(s, temp + s[index], index + 1);
+    let notTake = sequence(s, temp, index + 1);
+
+    return take.concat(notTake);
+}
+
+x = sequence("abc", "", 0);
+
+console.log(x);  
